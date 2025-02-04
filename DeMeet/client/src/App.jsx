03dc1a08466e0ce15c3ce3a748127d979e948ac3
Shubham singh <shubham1041singh@gmail.com>
2025-02-05 00:17:21 +0530

    Add registration form to authentication page

--- DeMeet/client/src/App.jsx
+++ DeMeet/client/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Auth/>} />
+        
       </Routes>
     </Router>
   );
--- DeMeet/client/src/pages/Auth.jsx
+++ DeMeet/client/src/pages/Auth.jsx
@@ -1,10 +1,12 @@
 import LoginPage from "@/components/auth/Login-form";
+import { RegisterForm } from "@/components/auth/SignupForm";
 import React from "react";
 
 const Auth = () => {
   return (
     <div>
       <LoginPage />
+      <RegisterForm/>
     </div>
   );
 };
