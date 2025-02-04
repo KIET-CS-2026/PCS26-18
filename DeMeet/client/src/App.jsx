688ec6e005b5bb867e702473380e4fbb21cdeba1
Shubham singh <shubham1041singh@gmail.com>
2025-02-04 23:57:07 +0530

    Add authentication page with login form and routing

--- DeMeet/client/src/App.jsx
+++ DeMeet/client/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Auth/>} />
       </Routes>
     </Router>
   );
---
+++ DeMeet/client/src/pages/Auth.jsx
@@ -0,0 +1,12 @@
+import LoginPage from "@/components/auth/Login-form";
+import React from "react";
+
+const Auth = () => {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+};
+
+export default Auth;
