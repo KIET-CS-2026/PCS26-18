1d4cd4eeb53ee3874b42019603b68c2e313d91e0
Rishika-993 <rishikaagrawal22@gmail.com>
2025-07-27 23:54:44 +0530

    Wallet connection

--- DeMeet/client/src/App.jsx
+++ DeMeet/client/src/App.jsx
@@ -10,6 +10,29 @@ import AuthRoute from "./components/AuthRoute";
 import Footer from "./components/Footer/Footer";
 import Room from "./pages/Room";
 import { Toaster } from "sonner";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  // GlowWalletAdaptor,
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  // SlopeWalletAdaptor,
+  // TorusWalletAdaptor
+} from "@solana/wallet-adapter-wallets";
+import React, { useMemo } from "react";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+const network = WalletAdapterNetwork.Devnet;
+
+const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
 
 function App() {
   return (
@@ -69,3 +92,25 @@ function App() {
 }
 
 export default App;
+
+const Context = ({ children }) => {
+  const endpoint = "https://api.devnet.solana.com";
+  const wallets = useMemo(() => {
+    return [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
+  }, []);
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+const Content = () => {
+  return (
+    <div className="App">
+      <WalletMultiButton />
+    </div>
+  );
+};
